4fd289d2b4f8cd6b3053feeed1fa982a7792b360
KozelMV <kozelmv@gmail.com>
2017-05-05 20:58:32 +0300

    Added class the Ball

--- pong/pong/pong.cpp
+++ pong/pong/pong.cpp
@@ -89,13 +89,14 @@ public:
 	//Движение вниз
 	int goDown();
 };
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//+++++++++++++++++++++++++++++++++
 User::User(int ucX) {
+	cX = ucX;
 	for (int i = 0; i < length; i++) {
 		set_cursor_pos(ucX, ( location + i)); //respawn
-		std::cout << chFill;
-		cX = ucX;
+		std::cout << chFill;	
 	}
+	location++;
 	set_cursor_pos(1,27);
 }
 int User::goUp() {
@@ -103,10 +104,10 @@ int User::goUp() {
 		return 0;
 	else {
 		for (int i = 0; i < length; i++) {
-			set_cursor_pos(cX, (location + (i+1)));
-			std::cout << ' ';
+			set_cursor_pos(cX, (location + 1));
+			std::cout << '.';
 			set_cursor_pos(1, 27);
-			set_cursor_pos(cX, (location - i));
+			set_cursor_pos(cX, (location - 2));
 			std::cout << chFill;
 			set_cursor_pos(1, 27);
 		}
@@ -119,10 +120,10 @@ int User::goDown() {
 		return 0;
 	else {
 		for (int i = 0; i < length; i++) {
-			set_cursor_pos(cX, (location - (i + 1)));
-			std::cout << ' ';
+			set_cursor_pos(cX, (location - 1));
+			std::cout << '.';
 			set_cursor_pos(1, 27);
-			set_cursor_pos(cX, (location + i));
+			set_cursor_pos(cX, (location + 2));
 			std::cout << chFill;
 			set_cursor_pos(1, 27);
 		}
@@ -132,6 +133,169 @@ int User::goDown() {
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Ball {
+private:
+	char b_chFill = hor;
+	char chClear = ' ';
+	int speed = 15;													  // Wait millsec
+	enum Angle { angleX = 3, angleY = 1 };							  // The flying angle the ball 
+	enum Border { cY_Min = 7, cY_Max = 26, cX_Min = 5, cX_Max = 70 }; // The border of the ball
+	short cY, cX;													  // The ball coordinate
+public:
+	Ball();
+	Ball(int x, int y);
+	void show();
+	void clear();
+	
+	void going();
+	void goingTop();
+	void goingTopLef();
+	void goingDown();
+	void goingDownLef();
+	
+	int top_or_down();			// Test on walls. Top and down
+	bool switchTorD = 1;		// The ball going to top or down
+	bool right_or_leaft = 1;	// The ball going to right(true) or left(false)		
+
+	bool goalRight();
+	bool goalLeft();
+};
+//+++++++++++++++++++++++++++++++++
+Ball::Ball() {
+	cX = cX_Min + 2;
+	cY = cY_Min + 9;
+	show();
+}
+Ball::Ball(int x, int y) {
+	cX = x;
+	cY = y;
+	show();
+}
+void Ball::show() {
+	set_cursor_pos(cX, cY);
+	std::cout << b_chFill;
+	set_cursor_pos(1, 27);
+}
+void Ball::clear() {
+	wait(speed);
+	set_cursor_pos(cX, cY);
+	std::cout << chClear;
+	set_cursor_pos(1, 27);
+}
+
+int Ball::top_or_down() {
+	if(switchTorD) {
+		if ((cY - 1) != cY_Min) {
+			return 1;
+		}
+		else {
+			switchTorD = !switchTorD;
+			Beep(800, 4 / 1);
+			return 0;
+		}
+	}
+	else if (!switchTorD) {
+		if ((cY + 1) != cY_Max) {
+			return 2;
+		}
+		else {
+			switchTorD = !switchTorD;
+			Beep(800, 4 / 1);
+			return 3;
+		}
+	}
+	else {
+		std::cout << "Warning 5";
+		return 5;
+	}
+}
+
+bool Ball::goalRight() {
+	if (cX + 1 >= cX_Max)
+		return 1;
+	else
+		return 0;
+}
+bool Ball::goalLeft() {
+	if (cX - 1 <= cX_Min)
+		return 1;
+	else
+		return 0;
+}
+
+void Ball::going() {
+	if (switchTorD) { //Вверх
+		if (right_or_leaft) { //Движение в право
+			if (top_or_down() == 1) {
+				if (!goalRight()) {
+					goingTop();
+				}
+				else {	// Гол в правые варота с верху
+					right_or_leaft = false; // Меняем направление движения
+					Beep(800, 4 / 1);
+				}
+			}
+		}
+		else {// Движение в лево
+			if (top_or_down() == 1) {
+				if (!goalLeft()) { // Движение назад вверх
+					goingTopLef();
+				}
+				else {	// Гол в левые варота с верху
+					right_or_leaft = true; // Меняем направление движения
+					Beep(800, 4 / 1);
+				}
+			}
+		}
+	}
+	else if (!switchTorD) { // Движение вниз
+		if (right_or_leaft) {
+			if (top_or_down() == 2) {
+				if (!goalRight()) {
+					goingDown();
+				}
+				else { // Гол в правые ворота снизу
+					right_or_leaft = false; // Меняем направление движения
+					Beep(800, 4 / 1);
+				}
+			}
+		}
+		else { //Движение в лево
+			if (top_or_down() == 2) {
+				if (!goalLeft()) {
+					goingDownLef();
+				}
+				else {	// Гол в левые варота снизу
+					right_or_leaft = true; // Меняем направление движения
+					Beep(800, 4 / 1);
+				}
+			}
+		}
+	}
+
+}
+void Ball::goingTop() {
+	clear();
+	cX += angleX; cY-= angleY;
+	show();
+}
+void Ball::goingDown() {
+	clear();
+	cX += angleX; cY+= angleY;
+	show();
+}
+void Ball::goingTopLef() {
+	clear();
+	cX -= angleX; cY-= angleY;
+	show();
+}
+void Ball::goingDownLef() {
+	clear();
+	cX -= angleX; cY+=angleY;
+	show();
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 int main()
 {	
@@ -140,23 +304,36 @@ int main()
 
 	GamingField Game;
 	User player1(5), player2(70);
-	char gameControl;
+	Ball b,b1(45,25),b2(20,20),b3(15,15);
+	char gameControl = ' ';
+	bool user1 = true;
+	bool user2 = true;
+
 	do
 	{
-		gameControl = _getch();
-
-		switch (gameControl) {
-		case 'w': player1.goUp(); break;
-		case 's': player1.goDown(); break;
-		default: break;
-		}
-		gameControl = _getch();
+		b.going();
+		b1.going();
+		b2.going();
+		b3.going();
+		//gameControl = _getch();
+		//
+		//if (gameControl == 'w') {
+		//	player1.goUp();
+		////	gameControl = _getch();
+		//	if (gameControl == 'o')
+		//		player2.goUp();
+		//}
+		//	if (gameControl == 's')
+		//		player1.goDown();
+		//
+		////gameControl = _getch();
+		//	if (gameControl == 'o')
+		//		player2.goUp();
+		//	if (gameControl == 'l')
+		//		player2.goDown();
 		
-		switch (gameControl) {
-		case 'o': player2.goUp(); break;
-		case 'l': player2.goDown(); break;
-		default: break;
-		}
+
+
 	} while (gameControl != 'q');
 
 	Beep(294, 1000 / 8); //звук <windows.h>
